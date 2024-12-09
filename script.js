3316c65ce203b954920ae6761bff5a80bfaae985
[Sebastian Ochal] <[sebastianochal1@gmail.com]>
2024-12-09 23:55:00 +0000

    spent the day making another css background.will have to finish tomorrow as it is late

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const backgroundImage = document.getElementById("pattern")
+const backgroundImage = document.getElementById("stars")
 
-backgroundImage.onclick = dots;
+document.addEventListener("DOMContentLoaded",dots);
 
 function dots(){
-    for (let i = 0; i < 10; i++) {
-        const dot = document.createElement("div")
+    for (let i = 0; i < 200; i++) {
+        const dot = document.createElement("div");
         // dot position
-        dot.classList.add("circle")
-        backgroundImage.appendChild(dot)
+        dot.classList.add("circle");
+        backgroundImage.appendChild(dot);
         dot.style.top = (window.innerHeight * Math.random())+"px";
         dot.style.left = (window.innerWidth * Math.random())+"px";
         // dot size
@@ -18,10 +18,10 @@ function dots(){
         let brightness = (Math.floor(Math.random() * 10)+1)/10;
         dot.style.opacity = brightness;
 
-        if (i<5)
-            {const dot = document.createElement("div")
-                dot.classList.add("circle2")
-                backgroundImage.appendChild(dot)
+        if (i<75)
+            {const dot = document.createElement("div");
+                dot.classList.add("circle2");
+                backgroundImage.appendChild(dot);
                 dot.style.top = (window.innerHeight * Math.random())+"px";
                 dot.style.left = (window.innerWidth * Math.random())+"px";
                 
