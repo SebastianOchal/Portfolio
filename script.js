c7b4a4911df7ce54ae30165b451317e09cb180bb
[Sebastian Ochal] <[sebastianochal1@gmail.com]>
2024-12-20 11:51:00 +0000

    Completed version 1 of the portfolio

--- script.js
+++ script.js
@@ -33,13 +33,16 @@ function dots(){
     }
 }
 // * Slide Controller
-let slides = document.querySelectorAll(".slide")
+let slides = document.querySelectorAll(".slide");
+let infos = document.querySelectorAll(".info");
 let slideLoop = Array.from(slides);
+let infoDisplay = Array.from(infos);
+let animating = false;
 function displaySlides(){
     console.log(slideLoop);
     let shownSlides = slideLoop.slice(0,4);
-    shownSlides.forEach((slide, i) =>{
-        slide.classList.remove("slide0");
+    shownSlides.forEach((slide) =>{
+        slide.classList.remove(`slide0`);
         slide.classList.remove("slide1");
         slide.classList.remove("slide2");
         slide.classList.remove("slide3");
@@ -47,16 +50,27 @@ function displaySlides(){
     shownSlides.forEach((slide,i) =>{
         slide.classList.add(`slide${i}`);
     })
+    infoDisplay.forEach((info,i) =>{
+        info.classList.remove("info0");
+        if(i === 0){
+            info.classList.add("info0");
+        }
+    });
+    
 }
+
 function slideNext(){
     slideLoop.push(slideLoop.shift());
+    infoDisplay.push(infoDisplay.shift());
     displaySlides()
 }
 function slidePrev(){
     slideLoop.unshift(slideLoop.pop());
+    infoDisplay.unshift(infoDisplay.pop());
     displaySlides()
 }
 const next = document.getElementById("next")
 const back = document.getElementById("back")
+
 next.onclick = slideNext;
-back.onclick = slidePrev;   
+back.onclick = slidePrev; 
